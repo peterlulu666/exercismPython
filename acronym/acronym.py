c08da457a2a8b8b2cdbb0a679790f6ecc8517345
root <root@ide.ide.9719e147-a351-4b5f-9839-94e474164604.svc.cluster.local>
2019-12-17 03:33:52 +0000

    commit

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -27,9 +27,25 @@ def abbreviate(words):
     words_string = words_string.join(letter_list)
     words_list = []
     words_list = words_string.split()
+    # words_list = clean_words(words)
     acronym = ""
     for word in words_list:
         acronym = acronym + word[0]
     return acronym
 
 
+def clean_words(words):
+    words = str(words)
+    letter_list = []
+    for letter in words:
+        if letter.isalpha() or letter == " ":
+            letter_list.append(letter)
+        elif letter == "-":
+            letter_list.append(" ")
+        else:
+            letter_list.append("")
+    words_string = ""
+    words_string = words_string.join(letter_list)
+    words_list = []
+    words_list = words_string.split()
+    return words_list
